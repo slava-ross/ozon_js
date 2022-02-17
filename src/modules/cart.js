b6aea85fe30fa071700c55e379e208bcce332590
Yaroslav Yagodarov <slava-ross@yandex.ru>
2022-02-17 16:30:34 +0400

    FEAT: add 2-nd lesson activity

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -13,6 +13,7 @@ const cart = () => {
     
     cartBtn.addEventListener('click', openCart);
     cartCloseBtn.addEventListener('click', closeCart);
-};
 
-export default cart();
+}
+
+export default cart
