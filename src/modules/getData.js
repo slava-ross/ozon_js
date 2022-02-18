4576520f85a427ca6a5fa3ccb08cd37d473a529b
Yaroslav Yagodarov <slava-ross@yandex.ru>
2022-02-18 17:18:49 +0400

    FEAT: add 3-nd lesson activity

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,7 @@
-const getData = () => {
-    return fetch('https://ozone-js-test-default-rtdb.firebaseio.com/goods.json')
+const getData = (str) => {
+    return fetch(
+        `https://ozone-js-test-default-rtdb.firebaseio.com/goods.json?${str ? 'search=${str}' : ''}`
+    )
     .then((response) => {
         return response.json();
     })
